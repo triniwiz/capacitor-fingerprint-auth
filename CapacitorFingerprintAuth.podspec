43289c54c7a96a324b059ed529d10b74b88f8af6
Osei Fortune <fortune.osei@yahoo.com>
2018-06-14 11:42:00 -0400

    Build fix [IOS]

--- CapacitorFingerprintAuth.podspec
+++ CapacitorFingerprintAuth.podspec
@@ -1,7 +1,7 @@
 
   Pod::Spec.new do |s|
     s.name = 'CapacitorFingerprintAuth'
-    s.version = '0.0.1'
+    s.version = '0.0.2'
     s.summary = 'FingerPrint Auth'
     s.license = 'MIT'
     s.homepage = 'https://github.com/triniwiz/capacitor-fingerprint-auth'
--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -5,15 +5,18 @@ import LocalAuthentication
 var keychainItemIdentifier = "CapTouchIDKey"
 var keychainItemServiceName: String?;
 
+typealias JSObject = [String:Any]
+typealias JSArray = [JSObject]
+
 @objc(FingerPrintAuthPlugin)
 public class FingerPrintAuthPlugin: CAPPlugin {
     @objc func available(_ call:CAPPluginCall){
         let ctx = LAContext()
         var error:NSError?
         var obj = JSObject()
-        
+
         let has = ctx.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error)
-        
+
         if(error != nil){
             obj["any"] = false
             call.resolve(obj)
@@ -30,44 +33,44 @@ public class FingerPrintAuthPlugin: CAPPlugin {
         } else {
             obj["face"] = false
         }
-        
+
         call.resolve(obj)
     }
-    
+
     @objc func didFingerprintDatabaseChange(_ call: CAPPluginCall) {
-        
+
         var obj = JSObject()
-        
+
         let laContext = LAContext();
         var error: NSError?
         // we expect the dev to have checked 'isAvailable' already so this should not return an error,
         // we do however need to run canEvaluatePolicy here in order to get a non-nil evaluatedPolicyDomainState
-        
+
         let supported = !laContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error)
         if(error != nil){
             if (supported) {
                 call.reject("Not available");
                 return;
             }
-            
+
             // only supported on iOS9+, so check this.. if not supported just report back as false
             if #available(iOS 9.0, *) {
                 obj["value"] = false;
                 call.resolve(obj)
             }
-            
+
             let CapFingerprintDatabaseStateKey = "CapFingerprintDatabaseStateKey";
             let state = laContext.evaluatedPolicyDomainState;
             if (state != nil) {
-                
+
                 let stateStr = state?.base64EncodedString(options: Data.Base64EncodingOptions.init(rawValue: 0));
                 let standardUserDefaults = UserDefaults()
                 let storedState = standardUserDefaults.string(forKey:CapFingerprintDatabaseStateKey);
-                
+
                 // Store enrollment
                 standardUserDefaults.setValue(stateStr, forKey: CapFingerprintDatabaseStateKey)
                 standardUserDefaults.synchronize();
-                
+
                 // whenever a finger is added/changed/removed the value of the storedState changes,
                 // so compare agains a value we previously stored in the context of this app
                 let changed = storedState != nil && stateStr != storedState;
@@ -79,32 +82,32 @@ public class FingerPrintAuthPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func verify(_ call:CAPPluginCall){
         let message = call.getString("message") ?? "Scan your finger"
-        
+
         if (keychainItemServiceName == nil) {
-            
+
             let bundleID = Bundle.main.infoDictionary!["CFBundleIdentifier"]
             if(bundleID == nil){
                 call.reject("")
             }
             keychainItemServiceName =  bundleID as! String + ".TouchId"
         }
-        
+
         if (!FingerPrintAuthPlugin.createKeyChainEntry()) {
             verifyWithFallback(call);
             return;
         }
-        
+
         let query = NSMutableDictionary()
         query.setValue(kSecClassGenericPassword, forKey: kSecClass as String)
         query.setValue(keychainItemIdentifier, forKey: kSecAttrAccount as String)
         query.setValue(keychainItemServiceName, forKey: kSecAttrService as String)
-        
+
         // Note that you can only do this for Touch ID; for Face ID you need to tweak the plist value of NSFaceIDUsageDescription
         query.setValue(message, forKey: kSecUseOperationPrompt as String)
-        
+
         // Start the query and the fingerprint scan and/or device passcode validation
         let res = SecItemCopyMatching(query, nil);
         if (res == 0) {
@@ -112,9 +115,9 @@ public class FingerPrintAuthPlugin: CAPPlugin {
         } else {
             call.reject("");
         }
-        
+
     }
-    
+
     @objc func verifyWithFallback(_ call:CAPPluginCall){
         let cancelTitle = call.getString("cancelTitle") ?? ""
         let fallbackTitle = call.getString("fallbackTitle ") ?? ""
@@ -123,11 +126,11 @@ public class FingerPrintAuthPlugin: CAPPlugin {
         if(fallbackTitle != ""){
             ctx.localizedFallbackTitle = fallbackTitle
         }
-        
+
         if(cancelTitle != ""){
             ctx.localizedCancelTitle = cancelTitle
         }
-        
+
         ctx.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: message) { (success, error) in
             DispatchQueue.main.async {
                 if(success){
@@ -137,26 +140,26 @@ public class FingerPrintAuthPlugin: CAPPlugin {
                 }
             }
         }
-        
+
     }
-    
-    
+
+
     private static func createKeyChainEntry() -> Bool{
         let attributes = NSMutableDictionary();
         attributes.setValue(kSecClassGenericPassword, forKey: kSecClass as String)
         attributes.setValue(keychainItemIdentifier as String, forKey:kSecAttrAccount as String);
         attributes.setValue(keychainItemServiceName,forKey: kSecAttrService as String);
-        
+
         let accessControlRef = SecAccessControlCreateWithFlags(
             kCFAllocatorDefault,
             kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
             SecAccessControlCreateFlags.userPresence,
             nil
         );
-        
+
         if (accessControlRef == nil) {
             // print("Can't store identifier " + keychainItemIdentifier +  "in the KeyChain: " + accessControlError + ".");
-            
+
             return false;
         } else {
             attributes.setValue(accessControlRef, forKey: kSecAttrAccessControl as String)
@@ -164,7 +167,7 @@ public class FingerPrintAuthPlugin: CAPPlugin {
             let content = NSString.init(string:String(arc4random()))
             let nsData = content.data(using: String.Encoding.utf8.rawValue)
             attributes.setValue(nsData, forKey: kSecValueData as String)
-            
+
             SecItemAdd(attributes, nil);
             return true;
         }
